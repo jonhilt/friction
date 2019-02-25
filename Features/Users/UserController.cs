ba9b94bf0d0a21ec55dac2f1080a5111f25e0bd3
Jon Hilton <hilton.jon@gmail.com>
2019-02-25 18:55:42 +0000

    step 1

--- Features/Users/UserController.cs
+++ Features/Users/UserController.cs
@@ -5,24 +5,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace Friction.WebVS.Features.Users
 {
     public class UserController : Controller
-    {
-        private readonly IMediator mediator;
-
-        public UserController(IMediator mediator)
-        {
-            this.mediator = mediator;
-        }
-
+    {        
         public async Task<IActionResult> List()
         {
-            var list = await mediator.Send(new List.Query());
-            return View(list);
-        }
-
-        public async Task<IActionResult> Search(Search.Query query)
-        {
-            var results = await mediator.Send(query);
-            return View(results);
+            return View();
         }
     }
 }
